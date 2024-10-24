091fd5a893473d3905975d641fd7f3849ebd500b
Sinem Elmas <sinem34elmas@gmail.com>
2024-10-24 18:47:33 +0300

    Update Create_db.py

--- Create_db.py
+++ Create_db.py
@@ -3,7 +3,7 @@ import sqlite3
 
 df = pd.read_csv(r'C:\Users\Vostro\Desktop\Machine Failure Prediction using Sensor data.csv')
 
-conn = sqlite3.connect('Machine.db')
-df.to_sql('Machine', conn, if_exists='replace', index=False)
+conn = sqlite3.connect('machine_data.db')
+df.to_sql('machines', conn, if_exists='replace', index=False)
 conn.close()
 
